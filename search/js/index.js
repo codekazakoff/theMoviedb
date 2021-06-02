86567ea1962ae4b082cae03a0b911740093aaa04
shohchakiller@gmail.com <shohchakiller@gmail.com>
2021-06-02 08:54:52 +0500

    yana six ta add commit

--- search/js/index.js
+++ search/js/index.js
@@ -14,17 +14,18 @@ if (prevScrollpos > currentScrollPos) {
 prevScrollpos = currentScrollPos;
 }
 }
+
 const SEARCH_INPUT_VALUE = (localStorage.getItem("input-search-value"));
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=8a6efddbf519aa74be6e68f9ecfd443f&language=en-US&query=${SEARCH_INPUT_VALUE}&page=1&include_adult=false`;
 let search_localstore = document.getElementById('search_localstore');
-search_localstore.value = SEARCH_INPUT_VALUE;
 
+search_localstore.value = SEARCH_INPUT_VALUE;
 const API_KEY = '8a6efddbf519aa74be6e68f9ecfd443f';
 const tvShows = `https://api.themoviedb.org/3/search/tv?api_key=8a6efddbf519aa74be6e68f9ecfd443f&language=en-US&page=1&include_adult=false`;
 
-fetch(tvShows)
-.then(res => res.json())
-.then(data => console.log(data)); 
+// fetch(tvShows)
+// .then(res => res.json())
+// .then(data => console.log(data)); 
 
 const getSerachMoveiDb = async url => {
     try{
@@ -56,3 +57,32 @@ getSerachMoveiDb(SEARCH_URL);
 function click_photo(id){
   localStorage.setItem('movie_id', id);
 }
+
+
+let valueInput = document.getElementById('search_localstore');
+valueInput.addEventListener('change', updateValue);
+function updateValue(e) {
+  let sendValue = e.target.value;
+  localStorage.setItem('input-search-value',sendValue);
+  console.log(sendValue);
+  // valueInput.value = '';
+}
+
+
+
+$(document).ready(()=>{
+  $('.dws-progress-bar').circularProgress({
+      color: '#f016e5',
+      line_width : 18,
+      height : '350px',
+      width : '350px',
+      percent : 0,
+      starting_position : 25
+  }).circularProgress('animate',100,5000);
+})
+
+$(window).on('load', ()=>{
+  var $preloader = $('#preloader');
+  $preloader.delay(4800).fadeOut('slow');
+  
+})
